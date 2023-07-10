101e6644f79f802d9391e1c3e4bada89c2d7dead
Om Patel <ompatel@oms-macbook-pro.home>
2023-07-10 14:12:19 -0400

    changed model relationship, added basic views and templates

--- inventory/models.py
+++ inventory/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=25)
-    available_quantity = models.IntegerField(default=0)
+    available_quantity = models.FloatField(default=0)
     unit = models.CharField(max_length=10)
     price_per_unit = models.FloatField()
 
@@ -11,21 +11,22 @@ class Ingredient(models.Model):
         return f"{self.ingredient_name}: {self.price_per_unit}, {self.available_quantity}"
     
 
+  
+class MenuItem(models.Model):
+    item_name = models.CharField(max_length=25)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.item_name}: {self.price}"
 
 class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity_needed = models.CharField(max_length=5)
 
     def __str__(self):
         return f"{self.ingredient.ingredient_name}: Quantity: {self.quantity_needed} {self.ingredient.unit}s"
-    
-class MenuItem(models.Model):
-    item_name = models.CharField(max_length=25)
-    price = models.FloatField()
-    ingredients = models.ManyToManyField(RecipeRequirement)
-
-    def __str__(self):
-        return f"{self.item_name}: {self.price}"
+  
 
 
 class Purchase(models.Model):
@@ -33,4 +34,4 @@ class Purchase(models.Model):
     purchase_time = models.DateTimeField()
 
     def __str__(self):
-        return f"Purchase: {self.menu_item}, Time: {self.purchase_time}"
+        return f"Purchase: {self.menu_item.item_name}, Time: {self.purchase_time}"
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Ingredient
 
 # Create your views here.
+def home(request):
+    return render(request, "inventory/home.html")
+
+
+class IngredientList(ListView):
+    model = Ingredient
+    template_name = 'inventory/inventory.html'
